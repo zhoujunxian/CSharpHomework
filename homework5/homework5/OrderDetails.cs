f4623a48d7f4cb1c255e3a8827aac12d0c0ee9ab
zhoujunxian <877423589@qq.com>
2018-10-17 20:54:33 +0800

    homework5

--- homework5/homework5/OrderDetails.cs
+++ homework5/homework5/OrderDetails.cs
@@ -23,20 +23,6 @@ namespace program2
             //ojb.ChangeOrderByNum("2017302580097","123");
             //ojb.ChangeOrderByName("篮球", "排球");
             //ojb.ChangeOrderByBuyer("李四", "轩轩");
-            //foreach (string OrderNum in Order.OrderNum)
-            //{
-            //    Console.Write(OrderNum + "\t");
-            //}
-            //Console.WriteLine();
-            //foreach (string OrderName in Order.OrderName)
-            //{
-            //    Console.Write(OrderName + "\t");
-            //}
-            //Console.WriteLine();
-            //foreach (string OrderBuyer in Order.OrderBuyer)
-            //{
-            //    Console.Write(OrderBuyer + "\t");
-            //}
             //Console.WriteLine();
         }
     }
